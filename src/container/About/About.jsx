34699351caa10ae91ead2a54e524d1390c78ecb7
DevPed <pedromfr91@gmail.com>
2022-09-29 00:58:54 +0200

    About

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './About.scss';
+
+const abouts = [
+  {
+    title: 'Desarrollador Web',
+    description: 'Apasionado del Desarrollo Web y la innovación',
+    imgUrl: images.about01,
+  },
+  {
+    title: 'Desarrollador Web',
+    description: 'Apasionado del Desarrollo Web y la innovación',
+    imgUrl: images.about02,
+  },
+  {
+    title: 'Desarrollador Web',
+    description: 'Apasionado del Desarrollo Web y la innovación',
+    imgUrl: images.about03,
+  },
+  {
+    title: 'Desarrollador Web',
+    description: 'Apasionado del Desarrollo Web y la innovación',
+    imgUrl: images.about04,
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className='head-text'>
+        Convierto <span>tus IDEAS </span>
+        <br />
+        <span>en REALIDAD</span>
+      </h2>
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
