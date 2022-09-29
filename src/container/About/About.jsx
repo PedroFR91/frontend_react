f0a7950b77d7418f2fa2bfac5c95c31c2d8fbb8f
DevPed <pedromfr91@gmail.com>
2022-09-29 17:55:04 +0200

    Sanity & React

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,32 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 import './About.scss';
 
-const abouts = [
-  {
-    title: 'Desarrollador Web',
-    description: 'Apasionado del Desarrollo Web y la innovación',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Desarrollador Web',
-    description: 'Apasionado del Desarrollo Web y la innovación',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'Desarrollador Web',
-    description: 'Apasionado del Desarrollo Web y la innovación',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'Desarrollador Web',
-    description: 'Apasionado del Desarrollo Web y la innovación',
-    imgUrl: images.about04,
-  },
-];
-
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>
@@ -44,7 +30,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
